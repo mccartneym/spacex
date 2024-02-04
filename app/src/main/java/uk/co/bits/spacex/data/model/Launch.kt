3805a5871ca2a87bf197936154eaacebe409bd29
Mike McCartney <mike.mccartney1@dojo.tech>
2024-02-04 12:44:26 +0000

    Delete everything

--- app/src/main/java/uk/co/bits/spacex/data/model/Launch.kt
+++
@@ -1,8 +0,0 @@
-package uk.co.bits.spacex.data.model
-
-data class Launch(
-    val smallImageUrl: String?,
-    val success: Boolean?,
-    val name: String,
-    val date: String
-)
--- app/src/main/java/uk/co/bits/spacex/data/repository/LaunchesRepository.kt
+++
@@ -1,7 +0,0 @@
-package uk.co.bits.spacex.data.repository
-
-import uk.co.bits.spacex.data.model.Launch
-
-interface LaunchesRepository {
-    suspend fun getLaunches(): Result<List<Launch>>
-}
