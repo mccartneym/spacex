2153a16daa3dbba6ec3ea568b545518996ba4e0a
Mike McCartney <mike.mccartney@paymentsense.com>
2024-04-10 20:36:54 +0100

    Fix tests

--- app/src/test/java/uk/co/bits/spacex/ui/launches/LaunchListViewModelTest.kt
+++ app/src/test/java/uk/co/bits/spacex/ui/launches/LaunchListViewModelTest.kt
@@ -5,12 +5,9 @@ import com.google.common.truth.Truth.assertThat
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import uk.co.bits.spacex.LAUNCH_LIST
@@ -27,30 +24,16 @@ class LaunchListViewModelTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    //    private val listViewStateObserver: Observer<LaunchListViewState> = mockk(relaxed = true)
     private val getLaunches: GetLaunchesUseCase = mockk()
     private val provider: DispatcherProvider = TestDispatcherProvider()
-    private lateinit var sut: LaunchListViewModel
-
-    @Before
-    fun setUp() {
-        sut = LaunchListViewModel(getLaunches, provider)
-//        sut.listViewState.observeForever(listViewStateObserver)
-    }
+    private val sut: LaunchListViewModel by lazy { LaunchListViewModel(getLaunches, provider) }
 
     @Test
     fun `when loading launches results in error, then send error view state`() = runTest(provider.main()) {
         coEvery { getLaunches() } returns Result.failure(SpaceXLaunchesRepository.UnableToLoadLaunchesError())
 
-        sut.onStart(mockk())
-        advanceUntilIdle()
-
         val result = mutableListOf<LaunchListViewState>()
-        println("*** 1")
-        val result2 = sut.listViewState.first()
-        println("*** 1.5: $result2")
         sut.listViewState.take(2).toList(result)
-        println("*** 2")
 
         assertThat(result).isEqualTo(listOf(LaunchListViewState.ListLoading, LaunchListViewState.ListError))
     }
@@ -59,26 +42,20 @@ class LaunchListViewModelTest {
     fun `when loading launches results in empty list, then send empty view state`() = runTest(provider.main()) {
         coEvery { getLaunches() } returns Result.success(emptyList())
 
-        sut.onStart(mockk())
-        advanceUntilIdle()
+        val result = mutableListOf<LaunchListViewState>()
+        sut.listViewState.take(2).toList(result)
 
-//        verifySequence {
-//            listViewStateObserver.onChanged(LaunchListViewState.ListLoading)
-//            listViewStateObserver.onChanged(LaunchListViewState.ListEmpty)
-//        }
+        assertThat(result).isEqualTo(listOf(LaunchListViewState.ListLoading, LaunchListViewState.ListEmpty))
     }
 
     @Test
     fun `when loading results in list with content, then send has content view state`() = runTest(provider.main()) {
         coEvery { getLaunches() } returns Result.success(LAUNCH_LIST)
 
-        sut.onStart(mockk())
-        advanceUntilIdle()
+        val result = mutableListOf<LaunchListViewState>()
+        sut.listViewState.take(2).toList(result)
 
-//        verifySequence {
-//            listViewStateObserver.onChanged(LaunchListViewState.ListLoading)
-//            listViewStateObserver.onChanged(LaunchListViewState.ListHasContent(LAUNCH_LIST))
-//        }
+        assertThat(result).isEqualTo(listOf(LaunchListViewState.ListLoading, LaunchListViewState.ListHasContent(LAUNCH_LIST)))
     }
 
 }
