4476ddabcd733b83dc0a3efe341e963e93720d26
Mike McCartney <mike.mccartney@paymentsense.com>
2024-04-11 13:00:26 +0100

    Add strictmode

--- app/src/main/java/uk/co/bits/spacex/ui/App.kt
+++ app/src/main/java/uk/co/bits/spacex/ui/App.kt
@@ -1,6 +1,7 @@
 package uk.co.bits.spacex.ui
 
 import android.app.Application
+import android.os.StrictMode
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -9,10 +10,35 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         initLogging()
+        enableStrictMode()
     }
 
     private fun initLogging() {
         Timber.plant(Timber.DebugTree())
         Timber.d("Logging initialised")
+    }
+
+    private fun enableStrictMode() {
+        Timber.v("enableStrictMode")
+        val threadPolicy = StrictMode.ThreadPolicy.Builder()
+            .detectDiskReads()
+            .detectDiskWrites()
+            .detectAll()
+//            .detectNetwork()   // or .detectAll() for all detectable problems
+            .penaltyFlashScreen()
+            .penaltyDeath()
+            .penaltyLog()
+            .build()
+
+        StrictMode.setThreadPolicy(threadPolicy)
+
+        val vmPolicy = StrictMode.VmPolicy.Builder()
+            .detectLeakedSqlLiteObjects()
+//            .detectLeakedClosableObjects()
+            .penaltyLog()
+            .penaltyDeath()
+            .build()
+
+        StrictMode.setVmPolicy(vmPolicy)
     }
 }
